f678a6babefd6226f295b1a34bbf31d5975d58fe
felipe-u <felipe_uv@outlook.com>
2022-03-04 16:56:39 -0500

    Creación Activity Nueva Categoria

---
+++ app/src/main/java/com/example/storageapp/views/NuevaCategoriaActivity.java
@@ -0,0 +1,16 @@
+package com.example.storageapp.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.storageapp.R;
+
+public class NuevaCategoriaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nueva_categoria);
+    }
+}
