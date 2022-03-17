90509c68b2d95287e61334ef27bda835e9b0ccd1
Luis <ingluiss@bythewave.co>
2022-03-17 11:43:56 -0500

    Implementación Charts con MPAndroidCharts y Alertdialogs

--- app/src/main/java/com/example/storageapp/fragments/ReportsFragment.java
+++ app/src/main/java/com/example/storageapp/fragments/ReportsFragment.java
@@ -9,9 +9,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.storageapp.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 
 public class ReportsFragment extends Fragment {
+
+    LineChart lineChart;
 
     public ReportsFragment() {
         // Required empty public constructor
@@ -25,7 +35,28 @@ public class ReportsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reports, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_reports, container, false);
+
+        lineChart = (LineChart) rootView.findViewById(R.id.lineChart);
+
+        ArrayList<Entry> resultados = new ArrayList<>();
+        resultados.add(new Entry(2014, 420));
+        resultados.add(new Entry(2015, 589));
+        resultados.add(new Entry(2016, 700));
+        resultados.add(new Entry(2017, 205));
+        resultados.add(new Entry(2018, 490));
+        resultados.add(new Entry(2019, 999));
+        resultados.add(new Entry(2020, 120));
+
+        LineDataSet lineDataSet = new LineDataSet(resultados, "Resultados");
+        lineDataSet.setColor(R.color.red);
+        lineDataSet.setValueTextColor(R.color.black);
+        lineDataSet.setValueTextSize(16f);
+
+        LineData lineData = new LineData(lineDataSet);
+
+        lineChart.setData(lineData);
+
+        return rootView;
     }
 }
--- app/src/main/java/com/example/storageapp/views/CreateProductActivity.java
+++ app/src/main/java/com/example/storageapp/views/CreateProductActivity.java
@@ -1,10 +1,14 @@
 package com.example.storageapp.views;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.storageapp.R;
 
@@ -26,5 +30,30 @@ public class CreateProductActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(CreateProductActivity.this);
+        alertDialog.setTitle("Aviso!")
+                .setIcon(R.drawable.ic_baseline_warning_24)
+                .setMessage("¿Está seguro que desea cancelar el registro?")
+                .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(CreateProductActivity.this, InventarioActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(CreateProductActivity.this, "Has cancelado!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .create()
+                .show();
     }
 }
