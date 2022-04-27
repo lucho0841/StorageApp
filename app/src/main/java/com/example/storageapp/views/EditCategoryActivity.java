d34834312aaf6b26775737ff6561f95a0928214a
felipe-u <felipe_uv@outlook.com>
2022-04-27 18:00:09 -0500

    Refactoring

--- app/src/main/java/com/example/storageapp/views/EditCategoryActivity.java
+++ app/src/main/java/com/example/storageapp/views/EditCategoryActivity.java
@@ -1,19 +1,36 @@
 package com.example.storageapp.views;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
+import com.example.storageapp.Dataholder;
 import com.example.storageapp.R;
+import com.example.storageapp.model.CategoryModel;
 
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class EditCategoryActivity extends AppCompatActivity {
 
     String nombre, descripcion;
+    int pos;
     EditText nuevoNombreCat , nuevoDescCat;
+    Button btnEditarCategoria;
+    private ArrayList<CategoryModel> categories = Dataholder.getInstance().categories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +38,7 @@ public class EditCategoryActivity extends AppCompatActivity {
 
         nombre = getIntent().getStringExtra("nuevoNombreCategoria");
         descripcion = getIntent().getStringExtra("nuevoDescCategoria");
+        pos = getIntent().getIntExtra("posicion",0);
         setContentView(R.layout.activity_edit_category);
 
         nuevoNombreCat = (EditText) findViewById(R.id.nuevoNombreCategoria);
@@ -28,5 +46,19 @@ public class EditCategoryActivity extends AppCompatActivity {
 
         nuevoNombreCat.setText(nombre, TextView.BufferType.EDITABLE);
         nuevoDescCat.setText(descripcion, TextView.BufferType.EDITABLE);
+
+        btnEditarCategoria = (Button) findViewById(R.id.btnEditarCategoria);
+        btnEditarCategoria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nuevoNombre = nuevoNombreCat.getText().toString();
+                String nuevoDescripcion = nuevoDescCat.getText().toString();
+                categories.get(pos).setNombre(nuevoNombre);
+                categories.get(pos).setDescripcion(nuevoDescripcion);
+                finish();
+            }
+        });
+
     }
+
 }
