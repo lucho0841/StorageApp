e1920c20f8b2ea2a5fdef27add669c15cec5255d
juan calderon <juandikld@gmail.com>
2022-06-05 12:54:14 -0500

    Creación de categorías con conexión firebase

--- app/src/main/java/com/example/storageapp/views/NuevaCategoriaActivity.java
+++ app/src/main/java/com/example/storageapp/views/NuevaCategoriaActivity.java
@@ -9,18 +9,36 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.storageapp.R;
+import com.example.storageapp.model.CategoryModel;
+import com.example.storageapp.model.ProductModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.EmptyStackException;
 
 public class NuevaCategoriaActivity extends AppCompatActivity {
 
-    // Hacer validaciones en este archivo con textfield
-    // CAMBIAR edtNombreCategoria POR tfNombreCategoria
 
 
+    private DatabaseReference mData;
     EditText edtNombreNuevaCat;
     EditText edtDescNuevaCat;
+    EditText edtCodNuevaCat;
     Button btnCrearCategoria;
+    int id = 0;
+    private FirebaseAuth mAuth;
+    private CategoryModel category;
+    String nombreCategoria, codigoCategoria, descripcionCategoria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,26 +46,32 @@ public class NuevaCategoriaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_nueva_categoria);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mAuth = FirebaseAuth.getInstance();
 
         edtNombreNuevaCat = (EditText) findViewById(R.id.edtNombreCategoria);
         edtDescNuevaCat = (EditText) findViewById(R.id.edtDescCategoria);
-        btnCrearCategoria = (Button) findViewById(R.id.btnCrearCategoria);
+        edtCodNuevaCat = (EditText) findViewById(R.id.edtCodNuevaCat);
 
+        btnCrearCategoria = (Button) findViewById(R.id.btnCrearCategoria);
         btnCrearCategoria.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String nombre = edtNombreNuevaCat.getText().toString();
-                String descripcion = edtDescNuevaCat.getText().toString();
+                nombreCategoria = edtNombreNuevaCat.getText().toString();
+                descripcionCategoria = edtDescNuevaCat.getText().toString();
+                codigoCategoria = edtCodNuevaCat.getText().toString();
 
-                if (nombre.isEmpty()) {
+                category = new CategoryModel(mAuth.getCurrentUser().getUid(),codigoCategoria,nombreCategoria,descripcionCategoria, id);
+                crearCategoriaBD(category);
+
+                if (nombreCategoria.isEmpty()) {
                     edtNombreNuevaCat.setError("El nombre es requerido");
                     edtNombreNuevaCat.requestFocus();
                     return;
                 }
 
                 Intent intent = new Intent(NuevaCategoriaActivity.this, InventarioActivity.class);
-                intent.putExtra("nombreCategoria", nombre);
-                intent.putExtra("descripcionCategoria", descripcion);
+                intent.putExtra("nombreCategoria", nombreCategoria);
+                intent.putExtra("descripcionCategoria", descripcionCategoria);
                 startActivity(intent);
                 finish();
             }
@@ -62,5 +86,46 @@ public class NuevaCategoriaActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    private void crearCategoriaBD(CategoryModel category){
+        if (nombreCategoria.isEmpty() && codigoCategoria.isEmpty() ){
+            edtNombreNuevaCat.setError("El nombre es requerido");
+            edtNombreNuevaCat.requestFocus();
+            return;
+        }
+        if (codigoCategoria.isEmpty()){
+            edtCodNuevaCat.setError("El codigo es requerido");
+            edtCodNuevaCat.requestFocus();
+            return;
+        }
+
+        mData = FirebaseDatabase.getInstance().getReference("Category");
+        mData.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    long numHijos = snapshot.getChildrenCount();
+                    id = Integer.parseInt(String.valueOf(numHijos)) + 1;
+
+                    category.setProductoId(id);
+                    FirebaseDatabase.getInstance().getReference("Category")
+                            .child(String.valueOf(id))
+                            .setValue(category).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(NuevaCategoriaActivity.this, "La categoría ha sido creado con Éxito!!!", Toast.LENGTH_LONG).show();
+                                finish();
+                            }
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
